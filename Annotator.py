ea347f9664bff23b7ad9231e33f5fa715ff692d4
Amin Abbasloo <abbasloo@cne3002.ad01.caesar.de>
2018-02-23 13:42:25 +0100

    image annotator

---
+++ Annotator.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+
+from PyQt5.QtCore import QDir, QPoint, QRect, QSize, Qt
+from PyQt5.QtGui import QImage, QImageWriter, QPainter, QPen, qRgb
+from PyQt5.QtWidgets import (QAction, QApplication, QColorDialog, QFileDialog,
+        QInputDialog, QMainWindow, QMenu, QMessageBox, QWidget)
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
+
+
+class ScribbleArea(QWidget):
+    def __init__(self, parent=None):
+        super(ScribbleArea, self).__init__(parent)
+
+        self.setAttribute(Qt.WA_StaticContents)
+        self.modified = False
+        self.scribbling = False
+        self.myPenWidth = 8
+        self.myPenColor = Qt.green
+        self.image = QImage()
+        self.lastPoint = QPoint()
+        self.Owidth = 0
+        self.Oheight = 0
+
+    def openImage(self, fileName):
+        loadedImage = QImage()
+        if not loadedImage.load(fileName):
+            return False
+        self.Owidth = loadedImage.width()
+        self.Oheight = loadedImage.height()
+        loadedImage = loadedImage.scaled(loadedImage.width()*0.3, loadedImage.height()*0.3)
+        newSize = loadedImage.size().expandedTo(self.size())
+        self.resizeImage(loadedImage, newSize)
+        self.image = loadedImage
+        self.modified = False
+        self.update()
+        return True
+
+    def saveImage(self, fileName, fileFormat):
+        visibleImage = self.image
+        visibleImage = visibleImage.scaled(self.Owidth, self.Oheight)
+        if visibleImage.save(fileName, fileFormat):
+            self.modified = False
+            return True
+        else:
+            return False
+
+    def setPenColor(self, newColor):
+        self.myPenColor = newColor
+
+    def setPenWidth(self, newWidth):
+        self.myPenWidth = newWidth
+
+    def clearImage(self):
+        self.image.fill(qRgb(255, 255, 255))
+        self.modified = True
+        self.update()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.scribbling = True
+        if (event.buttons() & Qt.LeftButton) and self.scribbling:
+            self.drawTo(event.pos())
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        dirtyRect = event.rect()
+        painter.drawImage(dirtyRect, self.image, dirtyRect)
+
+    def resizeEvent(self, event):
+        if self.width() > self.image.width() or self.height() > self.image.height():
+            newWidth = max(self.width() + 128, self.image.width())
+            newHeight = max(self.height() + 128, self.image.height())
+            self.resizeImage(self.image, QSize(newWidth, newHeight))
+            self.update()
+
+        super(ScribbleArea, self).resizeEvent(event)
+
+    def drawTo(self, endPoint):
+        painter = QPainter(self.image)
+        painter.setPen(QPen(self.myPenColor, self.myPenWidth, Qt.SolidLine,
+                Qt.RoundCap, Qt.RoundJoin))
+        painter.drawPoint(endPoint)
+        self.modified = True
+        self.update()       
+
+    def resizeImage(self, image, newSize):
+        if image.size() == newSize:
+            return
+
+        newImage = QImage(newSize, QImage.Format_RGB32)
+        newImage.fill(qRgb(255, 255, 255))
+        painter = QPainter(newImage)
+        painter.drawImage(QPoint(0, 0), image)
+        self.image = newImage
+
+    def print_(self):
+        printer = QPrinter(QPrinter.HighResolution)
+
+        printDialog = QPrintDialog(printer, self)
+        if printDialog.exec_() == QPrintDialog.Accepted:
+            painter = QPainter(printer)
+            rect = painter.viewport()
+            size = self.image.size()
+            size.scale(rect.size(), Qt.KeepAspectRatio)
+            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
+            painter.setWindow(self.image.rect())
+            painter.drawImage(0, 0, self.image)
+            painter.end()
+
+    def isModified(self):
+        return self.modified
+
+    def penColor(self):
+        return self.myPenColor
+
+    def penWidth(self):
+        return self.myPenWidth
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.saveAsActs = []
+
+        self.scribbleArea = ScribbleArea()
+        self.setCentralWidget(self.scribbleArea)
+
+        self.createActions()
+        self.createMenus()
+
+        self.setWindowTitle("Annotator")
+        self.resize(500, 500)
+
+    def closeEvent(self, event):
+        if self.maybeSave():
+            event.accept()
+        else:
+            event.ignore()
+
+    def open(self):
+        if self.maybeSave():
+            fileName, _ = QFileDialog.getOpenFileName(self, "Open File",
+                    QDir.currentPath())
+            if fileName:
+                self.scribbleArea.openImage(fileName)
+                self.resize(self.scribbleArea.image.size())
+
+    def save(self):
+        action = self.sender()
+        fileFormat = action.data()
+        self.saveFile(fileFormat)
+
+    def penColor(self):
+        newColor = QColorDialog.getColor(self.scribbleArea.penColor())
+        if newColor.isValid():
+            self.scribbleArea.setPenColor(newColor)
+
+    def penWidth(self):
+        newWidth, ok = QInputDialog.getInt(self, "Annotator",
+                "Select pen width:", self.scribbleArea.penWidth(), 8, 50, 1)
+        if ok:
+            self.scribbleArea.setPenWidth(newWidth)
+
+    def about(self):
+        QMessageBox.about(self, "About Annotator",
+                "Doing image annotation")
+
+    def createActions(self):
+        self.openAct = QAction("&Open...", self, shortcut="Ctrl+O",
+                triggered=self.open)
+
+        for format in QImageWriter.supportedImageFormats():
+            format = str(format)[1:]
+            text = format[1:len(format)-1]
+
+            action = QAction(text, self, triggered=self.save)
+            action.setData(format)
+            self.saveAsActs.append(action)
+
+        self.printAct = QAction("&Print...", self,
+                triggered=self.scribbleArea.print_)
+
+        self.exitAct = QAction("E&xit", self, shortcut="Ctrl+Q",
+                triggered=self.close)
+
+        self.penColorAct = QAction("&Pen Color...", self,
+                triggered=self.penColor)
+
+        self.penWidthAct = QAction("Pen &Width...", self,
+                triggered=self.penWidth)
+
+        self.clearScreenAct = QAction("&Clear Screen", self, shortcut="Ctrl+L",
+                triggered=self.scribbleArea.clearImage)
+
+        self.aboutAct = QAction("&About", self, triggered=self.about)
+
+        self.aboutQtAct = QAction("About &Qt", self,
+                triggered=QApplication.instance().aboutQt)
+
+    def createMenus(self):
+        self.saveAsMenu = QMenu("&Save As", self)
+        for action in self.saveAsActs:
+            self.saveAsMenu.addAction(action)
+
+        fileMenu = QMenu("&File", self)
+        fileMenu.addAction(self.openAct)
+        fileMenu.addMenu(self.saveAsMenu)
+        #fileMenu.addAction(self.printAct)
+        fileMenu.addSeparator()
+        fileMenu.addAction(self.exitAct)
+
+        optionMenu = QMenu("&Options", self)
+        optionMenu.addAction(self.penColorAct)
+        optionMenu.addAction(self.penWidthAct)
+        optionMenu.addSeparator()
+        #optionMenu.addAction(self.clearScreenAct)
+
+        helpMenu = QMenu("&Help", self)
+        helpMenu.addAction(self.aboutAct)
+        #helpMenu.addAction(self.aboutQtAct)
+
+        self.menuBar().addMenu(fileMenu)
+        self.menuBar().addMenu(optionMenu)
+        self.menuBar().addMenu(helpMenu)
+
+    def maybeSave(self):
+        if self.scribbleArea.isModified():
+            ret = QMessageBox.warning(self, "Scribble",
+                        "The image has been modified.\n"
+                        "Do you want to save your changes?",
+                        QMessageBox.Save | QMessageBox.Discard |
+                        QMessageBox.Cancel)
+            if ret == QMessageBox.Save:
+                return self.saveFile('png')
+            elif ret == QMessageBox.Cancel:
+                return False
+
+        return True
+
+    def saveFile(self, fileFormat):
+        fileFormat = fileFormat[1:len(fileFormat)-1]
+        initialPath = QDir.currentPath() + '/untitled.' + fileFormat
+
+        fileName, _ = QFileDialog.getSaveFileName(self, "Save As", initialPath,
+                "%s Files (*.%s);;All Files (*)" % (fileFormat.upper(), fileFormat))
+        if fileName:
+            return self.scribbleArea.saveImage(fileName, fileFormat.upper())
+
+        return False
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+sys.exit(app.exec_())
